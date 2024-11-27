cceb654a8880cb379d0c0746f8feeab1855ac40a
Karim Sonbul <sonbul.karim@gmail.com>
2024-11-27 13:03:41 +0100

    opt: added thread count in project 1

--- Projekt 1/ATMSimulatorThread.java
+++ Projekt 1/ATMSimulatorThread.java
@@ -1,15 +1,11 @@
+import java.util.Random;
+
 public class ATMSimulatorThread {
     public static void main(String[] args) {
         Account account = new Account(1000);
 
-        Thread client1 = new Thread(new Client(account, 200));
-        Thread client2 = new Thread(new Client(account,  -500));
-        Thread client3 = new Thread(new Client(account,  -300));
-        Thread client4 = new Thread(new Client(account, 700));
-
-        client1.start();
-        client2.start();
-        client3.start();
-        client4.start();
+        for (int i = 0; i < 500; i++) {
+            new Thread(new Client(account, new Random().nextInt(0, 1000))).start();
+        }
     }
 }
--- Projekt 1/ATMSimulatorThreadPoolExecutor.java
+++ Projekt 1/ATMSimulatorThreadPoolExecutor.java
@@ -1,17 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class ATMSimulatorThreadPoolExecutor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Account account = new Account(1000);
-
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
-
-        executor.submit(new Client(account, 200));
-        executor.submit(new Client(account, -500));
-        executor.submit(new Client(account, -300));
-        executor.submit(new Client(account, 700));
-
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(500);
+        List<Callable<Void>> tasks = new ArrayList<>();
+        for (int i = 0; i < 500; i++) {
+            tasks.add(() -> {
+                new Client(account, new Random().nextInt(0, 1000)).run();
+                return null;
+            });
+        }
+        executor.invokeAll(tasks);
         executor.shutdown();
     }
 }
