015210c07375cd3de50e3bc6017e06af4232e48e
Małgorzata Sokół <malgorzata@e-sokol.pl>
2024-12-17 01:13:45 +0100

    Added more problem files

--- Projekt 2/ConvexHull/src/main/java/org/pwo/convexhull/Geometry/PointGenerator.java
+++ Projekt 2/ConvexHull/src/main/java/org/pwo/convexhull/Geometry/PointGenerator.java
@@ -11,7 +11,11 @@ public class PointGenerator {
         uniform, gaussian, clustered
     }
 
-    public static List<Point2D> generate(int number, DistributionType distribution, double param1, double param2) {
+    public static List<Point2D> generate(
+            int number,
+            DistributionType distribution,
+            double param1, double param2
+    ) {
         List<Point2D> result = new ArrayList<>();
         Random random = new Random();
 
@@ -55,5 +59,58 @@ public class PointGenerator {
             }
         }
         return result;
+    }
+
+    public static List<Point2D> generateBounded(
+            int number,
+            DistributionType distribution,
+            double param1, double param2,
+            double boundX, double boundY
+    ) {
+        List<Point2D> result = new ArrayList<>();
+        Random random = new Random();
+
+        switch (distribution) {
+            case uniform -> {
+                for (int i = 0; i < number; i++) {
+                    double span = param2 - param1;
+                    double x = random.nextDouble() * span + param1;
+                    double y = random.nextDouble() * span + param1;
+
+                    result.add(new Point2D.Double(fMod(x, boundX), fMod(y, boundY)));
+                }
+            }
+            case gaussian -> {
+                for (int i = 0; i < number; i++) {
+                    double x = random.nextGaussian() * param2 + param1;
+                    double y = random.nextGaussian() * param2 + param1;
+
+                    result.add(new Point2D.Double(fMod(x, boundX), fMod(y, boundY)));
+                }
+            }
+            case clustered -> {
+                int clusters = (int) Math.max(1, Math.sqrt((double) number / 10));
+                List<Point2D> centroids = new ArrayList<>();
+                for (int i = 0; i < clusters; i++) {
+                    Point2D c = new Point2D.Double(
+                            random.nextDouble() * (param2 - param1) + param1,
+                            random.nextDouble() * (param2 - param1) + param1
+                    );
+                    centroids.add(c);
+                }
+                for (int i = 0; i < number; i++) {
+                    Point2D clusterCenter = centroids.get(random.nextInt(clusters));
+                    double x = clusterCenter.getX() + param2 * 0.1 * random.nextGaussian();
+                    double y = clusterCenter.getY() + param2 * 0.1 * random.nextGaussian();
+                    result.add(new Point2D.Double(fMod(x, boundX), fMod(y, boundY)));
+                }
+
+            }
+        }
+        return result;
+    }
+
+    public static double fMod(double a, double b) {
+        return  a - Math.floor(a / b) * b;
     }
 }
--- Projekt 2/ConvexHull/src/main/java/org/pwo/convexhull/Main.java
+++ Projekt 2/ConvexHull/src/main/java/org/pwo/convexhull/Main.java
@@ -9,21 +9,30 @@ import java.util.List;
 public class Main {
 
 //    C:\Users\MS\Desktop\PWO\Projekt\Projekt 2\ConvexHull\src\main\resources\problem10.txt
+
+    // Problem sizes
+    // small: number < 50
+    // medium: 100 < number < 1000
+    // large: 2000 < number < 10000
+    // very_large: number > 2000
+
     public static void main(String[] args) {
 
-        int version = 11;
-        String filename = "C:\\Users\\MS\\Desktop\\PWO\\Projekt\\Projekt 2\\ConvexHull\\src\\main\\resources\\problem" + version + ".txt";
+        int version = 12;
+        String size = "large";
+        String filename = "C:\\Users\\MS\\Desktop\\PWO\\Projekt\\Projekt 2\\ConvexHull\\src\\main\\resources\\problems\\" +
+                 size + "\\problem" + version + "_" + size + ".txt";
 
-        List<Point2D> problem = PointGenerator.generate( 600,
-                PointGenerator.DistributionType.clustered,
-                0,
-                1200
+        List<Point2D> problem = PointGenerator.generateBounded( 5000,
+                PointGenerator.DistributionType.gaussian,
+                500, 200,
+                1600, 1000
         );
 
         try {
             PointOperations.saveToFile(problem, filename);
         } catch (Exception ex) {
-            System.out.println("Something went wrong :(");
+            System.out.println("Something went wrong: "+ ex.getMessage());
         }
     }
 }
